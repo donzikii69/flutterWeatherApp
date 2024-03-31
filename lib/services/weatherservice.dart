ecc6a2a91519961a53aeabcc182306ec33f885a8
zikran69 <zikzigi69@gmail.com>
2024-04-01 01:30:37 +0700

    feat[just start]

---
+++ lib/services/weatherservice.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/weathermodel.dart';
+
+class WeatherService {
+  // ignore: constant_identifier_names
+  static const BASE_URL = 'https://api.openweathermap.org/data/3.0/weather';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    // get permission dari user
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    // fecth lokasi terakhir user
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // convert lokasi ke daftar tempat
+    List<Placemark> placemark =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    // extract nama kota dari tempat yang pertama di tandai
+    String? city = placemark[0].locality;
+
+    return city ?? "";
+  }
+}
